1a10ff7b97847fb51d981e750ced868d9739ab2c
Rivaldo2117051054 <rivaldorr547@gmail.com>
2021-12-04 09:18:22 +0700

    menghitung luas sederhana

---
+++ hitung luas.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int p,l,luas;
+	cout<<"menghitung luas"<<endl;
+	cin>>p;
+	//masukan panjang
+	cin>>l;
+	//masukan lebar
+	
+	luas=p*l;
+	//panjang kali lebar
+	
+	cout<<"luasnya adalah = "<<luas<<endl;
+}
